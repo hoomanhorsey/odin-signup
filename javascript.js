1354e9e01e9f25a863d85ac8d50d67fb081a451a
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-11-12 23:43:17 +1100

    Reworking of html structure and css styling. Still difficulties with resizing.

---
+++ javascript.js
@@ -0,0 +1,44 @@
+addEventListener('DOMContentLoaded', () => {
+
+let pwd = document.querySelector('#password');
+let confirmPwd = document.querySelector('#confirmPassword');
+
+let subBtn = document.querySelector('button');
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    
+    let password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+
+    console.log(password, confirmPassword);
+    
+    if (password != confirmPassword) {
+        alert("passwords don't match");
+    } else {
+        alert("Passwords Match");
+    }
+    console.log('hi');
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
