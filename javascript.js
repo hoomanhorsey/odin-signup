1f616e92332ac27e3f43c55f19aa7f323101e023
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-11-14 12:18:28 +1100

    Completed, but password validation a bit buggy - combined html validation and JS

--- javascript.js
+++ javascript.js
@@ -2,28 +2,33 @@ addEventListener('DOMContentLoaded', () => {
 
 let pwd = document.querySelector('#password');
 let confirmPwd = document.querySelector('#confirmPassword');
-
 let subBtn = document.querySelector('button');
 
 
 
 let form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
+// form.addEventListener('submit', (e) => 
+subBtn.addEventListener('click', (e) =>
+
+{
     
     let password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
 
+    const noMatch = document.querySelector('.noMatch');
 
     console.log(password, confirmPassword);
-    
+    if ((!password === " ")|| (!confirmPassword ===" ")) {
+        noMatch.textContent = "*Please enter a password";
+        console.log(password, confirmPassword)
+    }
     if (password != confirmPassword) {
-        alert("passwords don't match");
-        let noMatch = document.querySelector('.noMatch');
-
-        noMatch.textContent = 'No Match';
+        
+        noMatch.textContent = "*Passwords don't match!";
     } else {
-        alert("Passwords Match");
+        noMatch.textContent = 'Passwords Match. Good job!';
+
     }
 
 
